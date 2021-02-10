10883bdd469d3d3a1e465a5addda84f0e6006093
Daria <redichkina.ds@phystech.edu>
2021-02-11 00:12:37 +0300

    1 ex. Last changing (get married)

--- DR_2semestr/main.cpp
+++ DR_2semestr/main.cpp
@@ -2,9 +2,19 @@
 #include <string>
 
 int main() {
-	std::cout << "Hello, world!";
-	std::cout << "I'm here to study";
-	std::cout << "Can I?";
-	std::cout << "smth new";
+	
+	//for 1 ex//
+	std::string a, b;
+	std::cout << "Please, newlyweds, enter your names\n";
+	std::cin >> a >> b;
+
+	int i = 0;
+	std::cout << "If you are ready to get married, press 1. If you are not ready, press 0\n";
+	std::cin >> i;
+	if (i == 1)
+		std::cout << a << " and " << b << " you are married now\n";
+	else
+		std::cout << "Come back later\n";
+
 	return 0;
 }
